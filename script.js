70e2e2cf8a18f246bc87bec9423f0d10a420d601
panda9903 <97980368+panda9903@users.noreply.github.com>
2023-05-30 20:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) =>{
+    button.addEventListener('click', (e)=>{
+        try{
+            if(e.target.innerHTML == '='){
+                string = eval(string);
+                document.querySelector('input').value = string;  
+            }
+            else if(e.target.innerHTML == 'C'){
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else{
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        catch{
+            document.querySelector('input').value = "Error. Press C.";
+            string = "";
+        }
+})
+});
